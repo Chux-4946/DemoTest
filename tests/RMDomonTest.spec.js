5e50ce61c1d94b82e4e0e06de59a8eb5dd53a4c7
Chukwunomnso Nwankwo <nomnso.c@gmail.com>
2025-03-01 17:46:45 +0000

    commen demo

---
+++ tests/RMDomonTest.spec.js
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com./');
+  await page.locator('[data-test="username"]').fill('problem_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.locator('[data-test="firstName"]').fill('test1');
+ // await page.locator('[data-test="lastName"]').fill('2');
+  let lastname= await page.locator('[data-test="lastName"]')
+
+  //await expect(lastname).toBeVisible();
+  await expect(lastname).toBeFalsy();
+  
+  await lastname.fill('John');
+
+  await page.close();
+
+  
+ 
+});
